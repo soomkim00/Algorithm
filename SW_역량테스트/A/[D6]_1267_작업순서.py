80b1bd82797b7ff5e4e9b68a68c5facc84ba41cd
sumin kim <cm14121453@gmail.com>
2025-02-12 23:12:43 +0900

    0212 commti

--- SW_역량테스트/A/[D6]_1267_작업순서.py
+++ SW_역량테스트/A/[D6]_1267_작업순서.py
@@ -1,33 +1,40 @@
-import sys
-
-# sys.stdin = open("sample_input.txt", "r")
-
-
-def dfs(graph, node, parents):
-    for p in parents[node - 1]:
-        if p not in visited:
-            dfs(graph, p, parents)
-    else:
-        if node not in visited:
-            visited.append(node)
-        for next in graph[node - 1]:
-            dfs(graph, next, parents)
-
+from collections import deque
 
 for tc in range(1, 11):
+    # 입력
     v, e = map(int, input().split())
     edges = list(map(int, input().split()))
-    graph = [[] for _ in range(v)]
-    parents = [[] for _ in range(v)]
-    start_list = [0] * v
 
+    # 각 정점의 자식 정점 정보를 저장할 리스트
+    # 부모 정점 개수를 count할 리스트
+    graph = [[] for _ in range(v + 1)]
+    parent_cnt = [0] * (v + 1)
+
+    # 인접 리스트 생성
+    # 추가되는 자녀 정점으로 count 리스트 갱신
     for i in range(0, 2 * e, 2):
-        graph[edges[i] - 1].append(edges[i + 1])
-        parents[edges[i + 1] - 1].append(edges[i])
-        start_list[edges[i + 1] - 1] += 1
+        graph[edges[i]].append(edges[i + 1])
+        parent_cnt[edges[i + 1]] += 1
 
-    start = start_list.index(0) + 1
-    visited = []
-    dfs(graph, start, parents)
+    dq = deque()
+    # count가 0 == 부모 정점이 없는 == 최상위 정점
+    # 최상위 정점들을 큐에 추가
+    for i in range(1, v + 1):
+        if parent_cnt[i] == 0:
+            dq.append(i)
 
-    print(f"#{tc}", *visited)
+    visit = []
+    # 큐에서 정점을 하나씩 꺼내면서
+    # 해당 정점 자녀 정점들의 count를 하나씩 감소
+    # 즉, 해당 정점의 부모중 하나를 방문했다
+    # count가 0인 정점 == 부모 정점을 모두 방문한 정점만 큐에 추가
+    # 꺼낸 정점을 리스트에 추가해서 방문경로 생성!
+    while dq:
+        next = dq.popleft()
+        for child in graph[next]:
+            parent_cnt[child] -= 1
+            if parent_cnt[child] == 0 and child not in dq:
+                dq.append(child)
+        visit.append(next)
+
+    print(f"#{tc}", *visit)
