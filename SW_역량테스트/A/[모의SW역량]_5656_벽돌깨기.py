eeedc3981a8e81c97b6499b5e0e28fa0fd0f704c
sumin kim <cm14121453@gmail.com>
2025-02-18 00:35:09 +0900

    0217 commit

--- SW_역량테스트/A/[모의SW역량]_5656_벽돌깨기.py
+++ SW_역량테스트/A/[모의SW역량]_5656_벽돌깨기.py
@@ -1,8 +1,34 @@
+from collections import deque
+
+
+#  dfs를 이용한 공 떨어뜨릴 위치 생성
+def drop_ball(n, w, h, i):
+    temp = list(map(list, bricks))
+    if i == n:
+        return
+    for k in range(w):
+        for r in range(h):
+            if temp[r][k] != 0:
+                break
+        else:
+            continue
+        break_brick(k)
+
+
+
+def break_brick(k):
+    pass
+
+
+
+
 T = int(input())
 
 for tc in range(1, T + 1):
     N, W, H = map(int, input().split())
     bricks = [list(map(int, input().split())) for _ in range(H)]
 
+    remain = []
+    drop_ball(N, W, H, 0)
 
     print(f'#{tc} ')
