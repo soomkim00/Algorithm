50f7bb00de0bcc71cf5b70959db1f511e60bf16c
seok <9526yu@naver.com>
2025-03-07 17:56:47 +0900

    0307 commit

--- SW_역량테스트/A/[D4]_2819_격자판의_숫자_이어_붙이기.py
+++ SW_역량테스트/A/[D4]_2819_격자판의_숫자_이어_붙이기.py
@@ -1,13 +1,26 @@
+# 중복이 가능한 dfs..
+# 숫자가 커지긴 한데, 격자판 사이즈가 4x4라서 할만함
+# 결과값의 중복을 제거한 후 총 개수를 반환해야 하므로 결과를 set()에 저장
+
+def dfs(r, c, depth, num):
+    if depth == 7:
+        result.add(num)
+        return
+    for dr, dc in delta:
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < 4 and 0 <= nc < 4:
+            dfs(nr, nc, depth + 1, num * 10 + board[nr][nc])
+
+
 T = int(input())
 
 delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 for tc in range(1, T + 1):
     board = [list(map(int, input().split())) for _ in range(4)]
-    num_set = []
+    result = set()
     for r in range(4):
         for c in range(4):
+            dfs(r, c, 1, board[r][c])
 
-
-
-    print(f"#{tc} ")
+    print(f"#{tc} {len(result)}")
--- SW_역량테스트/A/[모의SW역량]_2105_디저트_카페.py
+++ SW_역량테스트/A/[모의SW역량]_2105_디저트_카페.py
@@ -1,7 +1,25 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+
+def dfs(r, c):
+    return 1
+
+
 T = int(input())
+delta = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
 
 for t in range(1, T + 1):
     N = int(input())
     cafe = [list(map(int, input().split())) for _ in range(N)]
+    route_len = set()
+    # 각 지점에서 길이를 구해서
+    for r in range(N):
+        for c in range(N):
+            route = []
+            dfs(r, c)
+
+
 
     print(f"#{t} ")
