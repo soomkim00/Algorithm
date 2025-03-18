6fcbc6c55e5dc8a8645d75fdde937198e902de29
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-03-18 23:19:55 +0900

    [Silver V] Title: 단어 정렬, Time: 116 ms, Memory: 112632 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -2,14 +2,14 @@ import sys
 
 if __name__ == "__main__":
     N = int(input())
-    word = [[] for _ in range(51)]
+    word = [[] for _ in range(52)]
     for _ in range(N):
-        w = sys.stdin.readline().strip()
+        w = sys.stdin.readline()
         word[len(w)].append(w)
 
-    for i in range(51):
+    for i in range(52):
         if word[i]:
             word[i] = list(set(word[i]))
             word[i].sort()
             for w in word[i]:
-                print(w)
+                sys.stdout.write(w)
