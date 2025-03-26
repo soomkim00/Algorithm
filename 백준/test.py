360e72873096150b7a14047191eecc811c691c45
김수민 <gkgkgkgk22@naver.com>
2025-03-26 17:51:01 +0900

    0326 commit

--- 백준/test.py
+++ 백준/test.py
@@ -4,27 +4,9 @@ input = sys.stdin.readline
 
 
 def solve():
-    N, K = map(int, input().split())
+    N = int(input())
+    for _ in range(N):
 
-    visited = [0] * (N + 1)
-    cnt = 0
-    idx = 1
-    order = 1
-    result = []
-
-    while cnt < N:
-        if visited[idx]:
-            idx += 1
-        else:
-            if order == K:
-                visited[idx] = 1
-                cnt += 1
-                order = 1
-                result.append(idx)
-            else:
-                idx = (idx + 1) % (N + 1)
-                order += 1
-    print(result)
 
 
 if __name__ == "__main__":
