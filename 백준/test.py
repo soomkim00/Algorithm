38f1292edb1b0cc4bde8c0db7037da6fb526a1ed
김수민 <gkgkgkgk22@naver.com>
2025-04-29 17:29:02 +0900

    0429 commit

--- 백준/test.py
+++ 백준/test.py
@@ -4,30 +4,13 @@ input = sys.stdin.readline
 
 
 def solve():
-    K, N = map(int, input().split())
-    lines = [int(input()) for _ in range(K)]
-
-    # 이진탐색
-    l = 0
-    r = max(lines)
-    answer = 0
-    while l <= r:
-        mid = l + (r - l) // 2
-        count = 0
-
-        if mid == 0:
-            answer = 1
-            break
-
-        for line in lines:
-            count += line // mid
-        if count >= N:
-            answer = mid
-            l = mid + 1
-        else:
-            r = mid - 1
-    print(answer)
-    return
+    N = int(input())
+    num = list(int(input()) for _ in range(N))
+    num.sort()
+    print(round(sum(num)/N))
+    print(num[N//2])
+    print()
+    print(num[-1] - num[0])
 
 
 if __name__ == "__main__":
