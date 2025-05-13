6bf0e35a59f1d7c42a23f8fab6331b0f34a6d17f
김수민 <gkgkgkgk22@naver.com>
2025-05-13 15:19:04 +0900

    0513 commit

--- 백준/test.py
+++ 백준/test.py
@@ -7,14 +7,21 @@ def solve():
     T = int(input())
 
     def fibo_count(n):
-        nonlocal cnt
-
+        nonlocal cnt0, cnt1
+        if n == 0:
+            cnt0 += 1
+            return 0
+        elif n == 1:
+            cnt1 += 1
+            return 1
+        else:
+            return fibo_count(n - 1) + fibo_count(n - 2)
 
     for _ in range(T):
         n = int(input())
-        cnt0, cnt1 = 0
+        cnt0, cnt1 = 0, 0
         fibo_count(n)
-        print(cnt)
+        sys.stdout.write(str(cnt0) + ' ' + str(cnt1) + '\nl')
 
 
 if __name__ == "__main__":
