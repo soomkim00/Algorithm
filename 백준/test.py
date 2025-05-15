b9aaa4ed2ebc061290a329902bb3fc098ea4c991
김수민 <gkgkgkgk22@naver.com>
2025-05-15 17:40:17 +0900

    0515 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,14 +1,37 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 
 def solve():
     N = int(input())
-    count = 0
+    stairs = [0] + list(int(input()) for _ in range(N))
+    q = deque()
+    q.append((0, 0, 0))  # 현재 칸, 연속된 1 개수, 총점
+    memo = dict()
 
+    while q:
+        now, cnt, score = q.popleft()
 
-    print(count)
+        # 도착점 넘어갔는지, 연속된 세번인지
+        if now > N or (cnt >= 2 and now > 2):
+            continue
+
+        temp = score + stairs[now]
+
+        if now in memo and temp <= memo[now]:
+            continue
+
+        score = temp
+        memo[now] = score
+
+        # 전진
+        q.append((now + 1, cnt + 1, score))  # 1칸 전진
+        q.append((now + 2, 0, score))
+
+    print(memo)
+    print(memo[N])
 
 
 if __name__ == "__main__":
