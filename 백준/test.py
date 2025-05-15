7ba1076369bdcd8634a714a9d2b7345aee0dfbb0
김수민 <gkgkgkgk22@naver.com>
2025-05-15 14:37:11 +0900

    temp

--- 백준/test.py
+++ 백준/test.py
@@ -4,24 +4,11 @@ input = sys.stdin.readline
 
 
 def solve():
-    T = int(input())
+    N = int(input())
+    count = 0
 
-    def fibo_count(n):
-        nonlocal cnt0, cnt1
-        if n == 0:
-            cnt0 += 1
-            return 0
-        elif n == 1:
-            cnt1 += 1
-            return 1
-        else:
-            return fibo_count(n - 1) + fibo_count(n - 2)
 
-    for _ in range(T):
-        n = int(input())
-        cnt0, cnt1 = 0, 0
-        fibo_count(n)
-        sys.stdout.write(str(cnt0) + ' ' + str(cnt1) + '\nl')
+    print(count)
 
 
 if __name__ == "__main__":
