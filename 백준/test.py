faef741446a0e5f117522369c8bf748bb23c4089
sumin kim <cm14121453@gmail.com>
2025-05-31 19:50:16 +0900

    0531 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,38 +1,21 @@
 import sys
-from collections import deque
-from collections import deque
-
 input = sys.stdin.readline
 
-
 def solve():
-    N = int(input())
-    K = int(input())
-    computer = [[] for _ in range(N + 1)]
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        data = dict()
+        for _ in range(N):
+            name, category = input().split()
+            if category in data:
+                data[category] += 1
+            else:
+                data[category] = 1
+        data_arr = list(data.values())
 
-    for _ in range(K):
-        s, e = map(int, input().split())
-        computer[s].append(e)
-        computer[e].append(s)
-
-    q = deque()
-    q.append(1)
-    visited = [0] * (N + 1)
-    count = 0
-    while q:
-        now = q.popleft()
-        if visited[now]:
-            continue
-        visited[now] = 1
-        count += 1
-
-        for next in computer[now]:
-            q.append(next)
-
-    if count:
-        print(count - 1)
-    else:
-        print(count)
+        total = 0
+        total += sum(data_arr)
 
 
 if __name__ == "__main__":
