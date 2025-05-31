ba7aef33b385a1f478e76f145a0cbce740311207
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-05-31 17:47:35 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,29 +1,24 @@
 import sys
-
 input = sys.stdin.readline
 
-
 def solve():
-    N = int(input())
-    for _ in range(N):
-        number = int(input())
-        count = 0
+    T = int(input())
+    numbers = [int(input()) for _ in range(T)]
+    max_n = max(numbers)
 
-        def dfs(total):
-            nonlocal count
+    dp = [0] * (max_n + 1)
+    dp[0] = 1
 
-            if total > number:
-                return
-            elif total == number:
-                count += 1
-                return
+    for i in range(1, max_n + 1):
+        if i - 1 >= 0:
+            dp[i] += dp[i - 1]
+        if i - 2 >= 0:
+            dp[i] += dp[i - 2]
+        if i - 3 >= 0:
+            dp[i] += dp[i - 3]
 
-            for add_num in (1, 2, 3):
-                dfs(total + add_num)
-
-        dfs(0)
-        print(count)
-
+    for num in numbers:
+        print(dp[num])
 
 if __name__ == "__main__":
     solve()
