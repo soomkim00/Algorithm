4479dc504393e81dde0bf9332f2ddc741ac90c14
sumin kim <cm14121453@gmail.com>
2025-06-02 21:56:54 +0900

    0602 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,10 +1,22 @@
 import sys
-from datetime import date
+
 input = sys.stdin.readline
 
+
 def solve():
-    today = date.today()
-    print(today)
+    N, M = map(int, input().split())
+    data = list(map(int, input().split()))
+
+    for i in range(1, N):
+        data[i] += data[i-1]
+
+    for _ in range(M):
+        i, j = map(int, input().split())
+        idx_i, idx_j = i - 2, j - 1
+        if idx_i == -1:
+            print(data[idx_j])
+        else:
+            print(data[idx_j] - data[idx_i])
 
 
 if __name__ == "__main__":
