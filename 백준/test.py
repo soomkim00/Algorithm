fe9df29c6b6983ddff6f8b328568aa3f51c700b5
sumin kim <cm14121453@gmail.com>
2025-06-05 17:34:31 +0900

    0605 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,22 +1,43 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
+delta = ((-1, 0), (1, 0), (0, -1), (0, 1))  # 상 하 좌 우
+
 
 def solve():
-    N = int(input())
-    if N == 1:
-        print(1)
-        return
-    elif N == 2:
-        print(2)
-        return
-    tile = [0] * (N + 1)
-    tile[1] = 1
-    tile[2] = 2
-    for i in range(3, N+1):
-        tile[i] = tile[i-1] + tile[i-2]
-    print(tile[N] % 10007)
+    T = int(input())
+    for _ in range(T):
+        M, N, K = map(int, input().split())
+        board = [[0] * M for _ in range(N)]
+        visited = [[0] * M for _ in range(N)]
+
+        for _ in range(K):
+            c, r = map(int, input().split())
+            board[r][c] = 1
+
+        def bfs(kr, kc):
+            q = deque()
+            q.append((kr, kc))
+            while q:
+                tr, tc = q.popleft()
+                if visited[tr][tc]:
+                    continue
+                visited[tr][tc] = 1
+                for dr, dc in delta:
+                    nr, nc = tr + dr, tc + dc
+                    if 0 <= nr < N and 0 <= nc < M and board[nr][nc]:
+                        q.append((nr, nc))
+
+        count = 0
+        for r in range(N):
+            for c in range(M):
+                if not (visited[r][c]) and board[r][c]:
+                    count += 1
+                    bfs(r, c)
+
+        print(count)
 
 
 if __name__ == "__main__":
