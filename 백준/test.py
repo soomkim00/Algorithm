59c21344342853eb8d63ff82c64a2fa5d7fd123a
sumin kim <cm14121453@gmail.com>
2025-06-07 17:14:24 +0900

    0607 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,20 +1,11 @@
 import sys
-from heapq import heappush, heappop
 
 input = sys.stdin.readline
 
 
 def solve():
-    N = int(input())
-    data = []
-    for _ in range(N):
-        num = int(input())
-        if num:
-            heappush(data, num)
-        elif data:
-            print(heappop(data))
-        else:
-            print(0)
+    N, M = map(int, input().split())
+    print(abs(N - M))
 
 
 if __name__ == "__main__":
