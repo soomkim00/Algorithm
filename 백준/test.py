76bd83b65efe9bc787d579f700698be600440257
sumin kim <cm14121453@gmail.com>
2025-06-08 23:16:02 +0900

    0608 commit

--- 백준/test.py
+++ 백준/test.py
@@ -4,8 +4,13 @@ input = sys.stdin.readline
 
 
 def solve():
-    N, M = map(int, input().split())
-    print(abs(N - M))
+    data = set()
+    for _ in range(28):
+        data.add(int(input()))
+
+    for i in range(1, 31):
+        if i not in data:
+            print(i)
 
 
 if __name__ == "__main__":
