3225e91780169e34e96ba9a3b7ec17750abe4fff
sumin kim <cm14121453@gmail.com>
2025-06-11 10:06:51 +0900

    0611 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,42 +1,37 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
 
 def solve():
-    N = int(input())
-    data = [list(map(int, input().split())) for _ in range(N)]
+    N, M = map(int, input().split())
+    trees = list(map(int, input().split()))
 
-    q = deque()
-    q.append((0, 0, N - 1, N - 1))
-    count = [0, 0]
+    trees.sort(reverse=True)
 
-    while q:
-        r1, c1, r2, c2 = q.popleft()
-        s = data[r1][c1]
+    s = trees[0] - 1
+    e = 0
 
-        flag = True
-        for r in range(r1, r2 + 1):
-            for c in range(c1, c2 + 1):
-                if data[r][c] != s:
-                    flag = False
-                    break
-            if not flag:
+    while s >= e:
+        H = (s + e) // 2
+        total = 0
+        flag = 0
+
+        for tree in trees:
+            if H >= tree:
                 break
 
-        if flag:
-            count[s] += 1
-        else:
-            mid_r = (r1 + r2) // 2
-            mid_c = (c1 + c2) // 2
-            q.append((r1, c1, mid_r, mid_c))
-            q.append((r1, mid_c + 1, mid_r, c2))
-            q.append((mid_r + 1, c1, r2, mid_c))
-            q.append((mid_r + 1, mid_c + 1, r2, c2))
+            total += tree - H
 
-    print(count[0])
-    print(count[1])
+            if total >= M:
+                flag = 1
+                break
+        if flag:  # 현재 H가 충분히 낮다 -> 높여보자
+            e = H + 1
+        else:
+            s = H - 1
+
+    print(s)
 
 
 if __name__ == "__main__":
