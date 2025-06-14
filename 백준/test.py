ceebda00aa915410a8831772f70f5e3cf7b170fc
sumin kim <cm14121453@gmail.com>
2025-06-14 21:52:09 +0900

    0614 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,6 +1,5 @@
 import sys
-from heapq import heappop, heappush
-from pprint import pprint
+from collections import deque
 
 input = sys.stdin.readline
 
@@ -8,43 +7,37 @@ delta = ((-1, 0), (1, 0), (0, -1), (0, 1))  # 상 하 좌 우
 
 
 def solve():
-    n, m = map(int, input().split())
-    data = [list(map(int, input().split())) for _ in range(n)]
-    dist = [[float('inf')] * m for _ in range(n)]
+    M, N = map(int, input().split())
+    data = [list(map(int, input().split())) for _ in range(N)]
 
-    sr, sc = 0, 0
+    q = deque()
+    remain = 0
 
-    for r in range(n):
-        for c in range(m):
-            if data[r][c] == 2:
-                sr, sc = r, c
+    for r in range(N):
+        for c in range(M):
+            if data[r][c] == 1:
+                q.append((r, c, 0))
             elif data[r][c] == 0:
-                dist[r][c] = 0
+                remain += 1
 
-    dist[sr][sc] = 0
+    while q:
+        tr, tc, day = q.popleft()
 
-    h = []
-    heappush(h, (0, sr, sc))
-
-    while h:
-        now_dist, tr, tc = heappop(h)
-
-        if dist[tr][tc] < now_dist:
-            continue
+        if remain == 0:
+            print(day)
+            return
 
         for dr, dc in delta:
             nr, nc = tr + dr, tc + dc
-            if 0 <= nr < n and 0 <= nc < m and data[nr][nc] and now_dist + 1 < dist[nr][nc]:
-                dist[nr][nc] = now_dist + 1
-                heappush(h, (now_dist + 1, nr, nc))
+            if 0 <= nr < N and 0 <= nc < M and data[nr][nc] == 0:
+                data[nr][nc] = 1  # ← 여기서 반드시 표시해 줍니다
+                remain -= 1
+                if remain == 0:  # ← 마지막 토마토가 익은 즉시
+                    print(day + 1)  # day+1 을 출력하고
+                    return
+                q.append((nr, nc, day + 1))
 
-    for r in range(n):
-        for c in range(m):
-            if dist[r][c] == float('inf'):
-                dist[r][c] = -1
-
-    for r in range(n):
-        print(*dist[r])
+    print(-1)
 
 
 if __name__ == '__main__':
