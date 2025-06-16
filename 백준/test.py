90b82f76e8293f8070f0b408d1157403da9ac938
sumin kim <cm14121453@gmail.com>
2025-06-16 09:51:02 +0900

    0616 commit

--- 백준/test.py
+++ 백준/test.py
@@ -5,8 +5,21 @@ input = sys.stdin.readline
 
 def solve():
     n = int(input())
-    dp = [0] * n
+    if n == 1:
+        print(1)
+        return
+    elif n == 2:
+        print(3)
+        return
+    dp = [0] * (n+1)
 
+    dp[1] = 1
+    dp[2] = 3
+
+    for i in range(3, n+1):
+        dp[i] = dp[i - 1] + 2 * dp[i - 2]
+
+    print(dp[n] % 10007)
 
 
 if __name__ == '__main__':
