f204198e859e29f2fb323218e6979aa731b37208
sumin kim <cm14121453@gmail.com>
2025-06-19 11:52:34 +0900

    0619 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,33 +1,13 @@
 import sys
+from heapq import heappop, heappush
 
 input = sys.stdin.readline
 
 
 def solve():
-    data = input()
-
-    temp = ''
-    flag = 1  # 1:pos, 0:neg
-
-    result = 0
-    for d in data:
-        if d not in ('-', '+'):
-            temp += d
-        else:
-            if flag:
-                if d == '-':
-                    flag = 0
-                result += int(temp)
-            else:
-                result -= int(temp)
-            temp = ''
-
-    if flag:
-        result += int(temp)
-    else:
-        result -= int(temp)
-
-    print(result)
+    N, M, B = map(int, input().split())
+    data = [list(map(int, input().split())) for _ in range(N)]
+    print(data)
 
 
 if __name__ == '__main__':
