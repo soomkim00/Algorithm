61776f220d96a1240eee58cef3394f45d18815eb
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-06-20 10:51:32 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 180 ms, Memory: 15176 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
