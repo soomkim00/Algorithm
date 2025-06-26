4d7ae5a2c24e7f7ed20129dca9f7ff7f0bd4dc38
sumin kim <cm14121453@gmail.com>
2025-06-26 11:19:58 +0900

    0626 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,40 +1,11 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
-delta = ((-1, 0), (1, 0), (0, -1), (0, 1))
-
 
 def solve():
-    N = int(input())
-    data = [list(map(int, input().strip())) for _ in range(N)]
-    counts = []
-
-    def bfs(sr, sc):
-        q = deque()
-        q.append((sr, sc))
-        data[sr][sc] = 0
-        count = 0
-        while q:
-            tr, tc = q.popleft()
-            count += 1
-            for dr, dc in delta:
-                nr, nc = tr + dr, tc + dc
-                if 0 <= nr < N and 0 <= nc < N and data[nr][nc]:
-                    data[nr][nc] = 0
-                    q.append((nr, nc))
-        counts.append(count)
-
-    for r in range(N):
-        for c in range(N):
-            if data[r][c]:
-                bfs(r, c)
-
-    counts.sort()
-    print(len(counts))
-    for cnt in counts:
-        print(cnt)
+    data = input()
+    print(data.swapcase())
 
 
 if __name__ == '__main__':
