a65b21ac9e142533fb9e09b348833c56411b9699
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-06-28 20:07:15 +0900

    [Silver I] Title: 절댓값 힙, Time: 864 ms, Memory: 116244 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,21 @@
+import sys
+from heapq import heappop, heappush
+
+
+def solve():
+    N = int(input())
+    heap = []
+
+    for _ in range(N):
+        x = int(input())
+        if x:
+            heappush(heap, (abs(x), x))
+        else:
+            if heap:
+                print(heappop(heap)[1])
+            else:
+                print(0)
+
+
+if __name__ == '__main__':
+    solve()
