3847a32382ea2fb94c32a0702e75646b3cc3757b
sumin kim <cm14121453@gmail.com>
2025-06-28 20:19:58 +0900

    0628 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,28 +1,23 @@
 import sys
+from heapq import heappop, heappush
+
 input = sys.stdin.readline
+
 
 def solve():
     N = int(input())
-    M = int(input())
-    S = input().strip()
+    heap = []
 
-    cnt = 0    # 연속된 "IOI" 패턴 수
-    ans = 0    # 최종 정답
-    i = 0
-    # i < M-2 인 이유: i, i+1, i+2 접근을 위해
-    while i < M - 2:
-        if S[i] == 'I' and S[i+1] == 'O' and S[i+2] == 'I':
-            cnt += 1
-            # cnt가 N에 딱 도달하면 한 번 추가, 이후 겹치게 세기 위해 cnt-- 해 줌
-            if cnt == N:
-                ans += 1
-                cnt -= 1
-            i += 2
+    for _ in range(N):
+        x = int(input())
+        if x:
+            heappush(heap, (abs(x), x))
         else:
-            cnt = 0
-            i += 1
+            if heap:
+                print(heappop(heap)[1])
+            else:
+                print(0)
 
-    print(ans)
 
 if __name__ == '__main__':
     solve()
