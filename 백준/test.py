2495673124f780d31f7994d1e6f64d66b3bdc2ab
sumin kim <cm14121453@gmail.com>
2025-06-29 22:30:31 +0900

    0629 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,22 +1,26 @@
 import sys
-from heapq import heappop, heappush
 
 input = sys.stdin.readline
 
+grade = {
+    "A+": 4.3,
+    "A0": 4.0,
+    "A-": 3.7,
+    "B+": 3.3,
+    "B0": 3.0,
+    "B-": 2.7,
+    "C+": 2.3,
+    "C0": 2.0,
+    "C-": 1.7,
+    "D+": 1.3,
+    "D0": 1.0,
+    "D-": 0.7,
+    "F": 0.0,
+}
+
 
 def solve():
-    N = int(input())
-    heap = []
-
-    for _ in range(N):
-        x = int(input())
-        if x:
-            heappush(heap, (abs(x), x))
-        else:
-            if heap:
-                print(heappop(heap)[1])
-            else:
-                print(0)
+    print(grade[input().strip()])
 
 
 if __name__ == '__main__':
