6f8b3f14209baeb8a003bfe08c21c09e40ac3eea
sumin kim <cm14121453@gmail.com>
2025-07-02 23:18:54 +0900

    0702 commit

--- 백준/test.py
+++ 백준/test.py
@@ -1,13 +1,30 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 
 def solve():
-    T = int(input())
-    for _ in range(T):
-        data = input().strip()
-        print(data[0] + data[-1])
+    N = int(input())
+    adj_list = [list(map(int, input().split())) for _ in range(N)]
+
+    result = [[0] * N for _ in range(N)]
+
+    def bfs(s):
+        q = deque()
+        q.append(s)
+        while q:
+            now = q.popleft()
+            for idx in range(N):
+                if adj_list[now][idx] and not result[s][idx]:
+                    result[s][idx] = 1
+                    q.append(idx)
+
+    for start in range(N):
+        bfs(start)
+
+    for r in result:
+        print(*r)
 
 
 if __name__ == '__main__':
