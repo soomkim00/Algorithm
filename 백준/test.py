09048211634ba2db8fa034b7e4394b3d5b0f3ed2
sumin kim <cm14121453@gmail.com>
2025-07-05 20:57:58 +0900

    0705 commit

--- 백준/test.py
+++ 백준/test.py
@@ -3,6 +3,8 @@ from collections import deque
 
 input = sys.stdin.readline
 
+delta = ((-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1))
+
 
 def solve():
     M, N, H = map(int, input().split())
@@ -11,12 +13,36 @@ def solve():
         layer = [list(map(int, input().split())) for _ in range(N)]
         data.append(layer)
 
-    tomatos = set()
-    count = 0
+    q = deque()
+    remain = 0
     for d in range(H):
         for r in range(N):
             for c in range(M):
-                if 
+                if data[d][r][c] == 1:
+                    q.append((d, r, c, 0))
+                elif data[d][r][c] == 0:
+                    remain += 1
+
+    if not remain:
+        print(0)
+        return
+
+    def bfs():
+        nonlocal remain
+        while q:
+            td, tr, tc, day = q.popleft()
+            for dd, dr, dc in delta:
+                nd, nr, nc = td + dd, tr + dr, tc + dc
+                if 0 <= nd < H and 0 <= nr < N and 0 <= nc < M and not data[nd][nr][nc]:
+                    data[nd][nr][nc] = 1
+                    remain -= 1
+                    if not remain:
+                        return day + 1
+                    q.append((nd, nr, nc, day + 1))
+        return -1
+
+    print(bfs())
+
 
 if __name__ == '__main__':
     solve()
