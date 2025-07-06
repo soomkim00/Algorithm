9d629943ea71b4ceda013a546cacd126a9f2c865
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-07-06 22:43:22 +0900

    [Bronze V] Title: A/B, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -6,14 +8,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        double c = (double) a / b;
 
-        bw.write(c + "\n");
-        bw.flush();
-
+        System.out.println((double) a / b);
     }
 }
