e1c1138558bc4ef7ecae15ea88feaf51836c9739
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-07-07 09:13:40 +0900

    [Bronze V] Title: 구구단, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,16 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 1; i < 10; i++) {
             System.out.printf("%d * %d = %d\n", N, i, N * i);
         }
-
     }
 }
