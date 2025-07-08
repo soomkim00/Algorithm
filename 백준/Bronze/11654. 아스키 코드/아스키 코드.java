7a8318c3f021b9b4d87af4cf9186998bf2ba2be0
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-07-08 09:31:57 +0900

    [Bronze V] Title: 아스키 코드, Time: 112 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        char ch = input.charAt(0);
+        int ascii = input.charAt(0);
+
+        System.out.println(ascii);
+
+    }
+}
