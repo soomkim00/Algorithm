4a7663a0c6fead97434c2ab755f977b0557f39d4
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-07-10 08:30:04 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,25 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String line = br.readLine();
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-        String[] tokens = line.split("\\s+");
-
-        int total = 0;
-
-        for (int i = 0; i < tokens.length; i++) {
-            int num = Integer.parseInt(tokens[i]);
-            total += num * num;
-        }
-
-        int result = total % 10;
+        int result = (A * A + B * B + C * C + D * D + E * E) % 10;
         System.out.println(result);
-
-
     }
 }
