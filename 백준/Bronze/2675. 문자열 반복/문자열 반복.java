07d89b86da8d7c05491c29899a2ca3dd688615e5
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-07-09 09:50:32 +0900

    [Bronze II] Title: 문자열 반복, Time: 96 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -5,18 +5,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int T = Integer.parseInt(br.readLine());
 
         for (int i=0; i<T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int R = Integer.parseInt(st.nextToken());
             String S = st.nextToken();
-            for (char s : S.toCharArray()) {
-                for (int j=0; j<R; j++) {
-                    bw.write(s);
+            int len = S.length(), totalLen = R * len + 1;
+            StringBuilder sb = new StringBuilder(totalLen);
+            for (char c : S.toCharArray()) {
+                for (int j = 0; j < R; j++) {
+                    sb.append(c);
                 }
             }
-            bw.write("\n");
+            sb.append('\n');
+            bw.write(sb.toString());  // bw.write 호출 1회
 
         }
         bw.flush();
