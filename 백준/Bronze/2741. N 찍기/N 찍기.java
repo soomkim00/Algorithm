b45660823274bd01cbeb6633440f5e8d702d67c0
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-07-10 08:36:03 +0900

    [Bronze V] Title: N 찍기, Time: 776 ms, Memory: 31572 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,15 +1,14 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 1; i <= N; i++) {
-            bw.write(i + "\n");
+            System.out.println(i);
         }
-        bw.flush();
     }
 }
