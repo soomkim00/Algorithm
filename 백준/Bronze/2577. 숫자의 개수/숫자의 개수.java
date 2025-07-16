dd7eeddf2091be26ec77170b091eb944c5ee54cc
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-07-17 08:45:41 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -9,18 +9,33 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             result *= Integer.parseInt(br.readLine());
         }
-        int[] count = new int[10];
-        String sen = Integer.toString(result);
 
-        for (char c : sen.toCharArray()) {
-            int num = c - '0';
-            count[num]++;
-        }
+        int[] count = new int[10];
+        do {
+            count[result % 10]++;
+            result /= 10;
+        } while (result > 0);
+
+        StringBuilder sb = new StringBuilder(30);
 
         for (int n : count) {
-            bw.write(Integer.toString(n));
-            bw.newLine();
+            sb.append(n).append("\n");
         }
-        bw.flush();
+        System.out.println(sb);
+
+//
+//        int[] count = new int[10];
+//        String sen = Integer.toString(result);
+//
+//        for (char c : sen.toCharArray()) {
+//            int num = c - '0';
+//            count[num]++;
+//        }
+//
+//        for (int n : count) {
+//            bw.write(Integer.toString(n));
+//            bw.newLine();
+//        }
+//        bw.flush();
     }
 }
