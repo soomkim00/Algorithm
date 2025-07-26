51a10d38c74d56b9394d5cb0da4f4672c74f5a98
sumin kim <86261007+soomkim00@users.noreply.github.com>
2025-07-26 20:17:56 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,42 @@
+import sys
+from _collections import deque
+
+input = sys.stdin.readline
+
+
+def solve():
+    N, M = map(int, input().split())
+
+    moves = dict()
+    for _ in range(N + M):
+        s, e = map(int, input().split())
+        moves[s] = e
+
+    def bfs():
+        q = deque()
+        visited = [False] * 101
+        q.append((1, 0))  # position, count
+
+        while q:
+            pos, cnt = q.popleft()
+            visited[pos] = True
+            for step in range(1, 7):
+                next = pos + step
+                # 뱀이나 사다리 확인
+                if next in moves:
+                    next = moves[next]
+
+                # 종료 확인
+                if next == 100:
+                    return cnt + 1
+
+                # 다음 칸 접근
+                if next < 100 and not visited[next]:
+                    q.append((next, cnt + 1))
+                    visited[next] = True
+
+    print(bfs())
+
+
+if __name__ == '__main__':
+    solve()
