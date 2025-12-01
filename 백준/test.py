c2c33767f8d8c5cf21c2260937e0199009fefb0d
김수민 <gkgkgkgk22@naver.com>
2025-12-01 15:21:03 +0900

    1201 commit

--- 백준/test.py
+++ 백준/test.py
@@ -3,45 +3,16 @@ from collections import deque
 
 input = sys.stdin.readline
 
-delta = ((-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1))
-
 
 def solve():
-    M, N, H = map(int, input().split())
-    data = []
-    for _ in range(H):
-        layer = [list(map(int, input().split())) for _ in range(N)]
-        data.append(layer)
+    T = int(input())
+    for _ in range(T):
+        a, b = map(int, input().split())
+        visited = set()
+        q = deque()
 
-    q = deque()
-    remain = 0
-    for d in range(H):
-        for r in range(N):
-            for c in range(M):
-                if data[d][r][c] == 1:
-                    q.append((d, r, c, 0))
-                elif data[d][r][c] == 0:
-                    remain += 1
 
-    if not remain:
-        print(0)
-        return
-
-    def bfs():
-        nonlocal remain
-        while q:
-            td, tr, tc, day = q.popleft()
-            for dd, dr, dc in delta:
-                nd, nr, nc = td + dd, tr + dr, tc + dc
-                if 0 <= nd < H and 0 <= nr < N and 0 <= nc < M and not data[nd][nr][nc]:
-                    data[nd][nr][nc] = 1
-                    remain -= 1
-                    if not remain:
-                        return day + 1
-                    q.append((nd, nr, nc, day + 1))
-        return -1
-
-    print(bfs())
+    return
 
 
 if __name__ == '__main__':
