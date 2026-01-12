12a5e2a500530e8a9235350d02c9bed28a73de07
sumin kim <86261007+soomkim00@users.noreply.github.com>
2026-01-12 15:33:08 +0900

    [Silver V] Title: 나이순 정렬, Time: 496 ms, Memory: 44844 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -5,29 +5,42 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	public static class Member {
+		int age;
+		String name;
+
+		public Member(int age, String name) {
+			this.age = age;
+			this.name = name;
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
 
-		String[][] members = new String[n][2];
+		Member[] members = new Member[n];
 
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			String age = st.nextToken();
+			int age = Integer.parseInt(st.nextToken());
 			String name = st.nextToken();
 
-			members[i][0] = age;
-			members[i][1] = name;
+			members[i] = new Member(age, name);
 		}
 
 		Arrays.sort(members, (m1, m2) -> {
-			return Integer.parseInt(m1[0]) - Integer.parseInt(m2[0]);
+			return m1.age - m2.age;
 		});
 
+		StringBuilder sb = new StringBuilder();
+
 		for (int i = 0; i < n; i++) {
-			System.out.println(members[i][0] + " " + members[i][1]);
+			sb.append(members[i].age).append(" ").append(members[i].name).append('\n');
 		}
+
+		System.out.print(sb);
 	}
 }
