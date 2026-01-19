c4a4c610b47d8ba99114cee21af301bcbb5c51a9
sumin kim <86261007+soomkim00@users.noreply.github.com>
2026-01-19 15:35:57 +0900

    [Silver IV] Title: 스택, Time: 168 ms, Memory: 19516 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -15,28 +15,23 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String command = st.nextToken();
-			if (command.equals("push")) {
-				stack.push(Integer.parseInt(st.nextToken()));
-			} else if (command.equals("pop")) {
-				if (stack.isEmpty()) {
-					sb.append(-1).append('\n');
-				} else {
-					sb.append(stack.pop()).append('\n');
-				}
-			} else if (command.equals("size")) {
-				sb.append(stack.size()).append('\n');
-			} else if (command.equals("empty")) {
-				if (stack.isEmpty()) {
-					sb.append(1).append('\n');
-				} else {
-					sb.append(0).append('\n');
-				}
-			} else if (command.equals("top")) {
-				if (stack.isEmpty()) {
-					sb.append(-1).append('\n');
-				} else {
-					sb.append(stack.peek()).append('\n');
-				}
+
+			switch (command) {
+				case "push":
+					stack.push(Integer.parseInt(st.nextToken()));
+					break;
+				case "pop":
+					sb.append(stack.isEmpty() ? -1 : stack.pop()).append('\n');
+					break;
+				case "size":
+					sb.append(stack.size()).append('\n');
+					break;
+				case "empty":
+					sb.append(stack.isEmpty() ? 1 : 0).append('\n');
+					break;
+				case "top":
+					sb.append(stack.isEmpty() ? -1 : stack.peek()).append('\n');
+					break;
 			}
 		}
 
