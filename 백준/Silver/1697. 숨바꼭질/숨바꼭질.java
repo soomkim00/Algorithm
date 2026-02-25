edda2e769a886faa98693d18bebf15c0c292a395
sumin kim <86261007+soomkim00@users.noreply.github.com>
2026-02-25 15:43:22 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 19752 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -18,27 +19,25 @@ public class Main {
 			return;
 		}
 
-		boolean[] visited = new boolean[100001];
-		ArrayDeque<int[]> queue = new ArrayDeque<>();  // pos, count
+		int[] dist = new int[100001];
+		Arrays.fill(dist, -1);  // -1이면 미방문
+		ArrayDeque<Integer> queue = new ArrayDeque<>();  // pos
 
-		queue.offer(new int[] {n, 0});
-		visited[n] = true;
+		queue.offer(n);
+		dist[n] = 0;  // 방문 처리
 
 		while (!queue.isEmpty()) {
-			int[] current = queue.poll();
-
-			int pos = current[0];
-			int count = current[1];
+			int pos = queue.poll();
 
 			for (int nextPos : new int[] {pos + 1, pos - 1, pos * 2}) {
-				if (nextPos >= 0 && nextPos <= 100000 && !visited[nextPos]) {
+				if (nextPos >= 0 && nextPos <= 100000 && dist[nextPos] == -1) {
 					if (nextPos == k) {
-						System.out.println(count + 1);
+						System.out.println(dist[pos] + 1);
 						return;
 					}
 
-					visited[nextPos] = true;
-					queue.offer(new int[] {nextPos, count + 1});
+					dist[nextPos] = dist[pos] + 1;
+					queue.offer(nextPos);
 				}
 			}
 		}
