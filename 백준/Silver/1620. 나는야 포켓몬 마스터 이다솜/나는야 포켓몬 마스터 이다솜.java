8e7407929dc7100585ebc5270c6c4fbd0964bd6f
sumin kim <86261007+soomkim00@users.noreply.github.com>
2026-03-07 22:40:03 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 612 ms, Memory: 91696 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		String[] nameArray = new String[n];
+		Map<String, Integer> nameMap = new HashMap<>();
+
+		for (int i = 0; i < n; i++) {
+			String input = br.readLine();
+			nameArray[i] = input;
+			nameMap.put(input, i);
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < m; i++) {
+			String input = br.readLine();
+			if (isNumeric(input)) {
+				sb.append(nameArray[Integer.parseInt(input) - 1]).append('\n');
+			} else {
+				sb.append(nameMap.get(input) + 1).append('\n');
+			}
+		}
+
+		System.out.print(sb);
+	}
+
+	private static boolean isNumeric(String s) {
+		try {
+			Integer.parseInt(s);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+}
