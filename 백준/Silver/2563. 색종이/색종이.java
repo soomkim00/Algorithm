bb07def6dce932a781b2386a45cfadbe55413229
sumin kim <86261007+soomkim00@users.noreply.github.com>
2026-04-06 22:20:00 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] canvas = new boolean[100][100];
+        int totalArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!canvas[j][k]) {
+                        canvas[j][k] = true;
+                        totalArea++;
+                    }
+                }
+            }
+        }
+        System.out.println(totalArea);
+    }
+}
