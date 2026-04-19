951ec97e7605d243c0cf151da1363e1e3ab9fef6
sumin kim <86261007+soomkim00@users.noreply.github.com>
2026-04-19 22:27:56 +0900

    [Bronze II] Title: 소수, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int min = -1;
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
+                if (min == -1) {
+                    min = i;
+                }
+                sum += i;
+            }
+        }
+
+        if (min == -1) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
