31ceb33192eb0d0c94177f5df05564e9d7ccfd78
sumin kim <86261007+soomkim00@users.noreply.github.com>
2026-06-22 21:59:34 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 1.16 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+	public int solution(int[] array, int height) {
+		int answer = 0;
+
+		Arrays.sort(array);
+
+		for (int i = array.length - 1; i >= 0; i--) {
+			if (array[i] > height)
+				answer++;
+			else
+				break;
+		}
+
+		return answer;
+	}
+}
