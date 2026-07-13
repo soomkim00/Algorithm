f712fcc8bf09ec0f55b057ff4c6bcaa5dfa6303e
sumin kim <86261007+soomkim00@users.noreply.github.com>
2026-07-13 09:30:05 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        for (int i = 5; i >= 1; i -= 2) {
+            answer += hp / i;
+            hp = hp % i;
+        }
+        return answer;
+    }
+}
