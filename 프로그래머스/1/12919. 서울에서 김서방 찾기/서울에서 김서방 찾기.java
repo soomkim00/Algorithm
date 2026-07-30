a016bcc8fc1dfdc659352e1f96577bfb130670bd
sumin kim <86261007+soomkim00@users.noreply.github.com>
2026-07-30 09:52:01 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 2.74 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int pos = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                pos = i;
+                break;
+            }
+        }
+        return "김서방은 " + pos + "에 있다";
+    }
+}
