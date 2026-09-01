db4808db3ec4be5733ed4c623ae92e25918addfd
sumin kim <86261007+soomkim00@users.noreply.github.com>
2026-09-01 21:12:47 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.82 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                sb.append(i);
+            }
+        }
+        StringBuilder reverse = new StringBuilder(sb).reverse();
+        
+        return (sb.append("0").append(reverse)).toString();
+    }
+}
